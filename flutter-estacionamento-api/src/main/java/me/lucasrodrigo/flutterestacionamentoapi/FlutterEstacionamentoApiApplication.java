3503cce14059bc00f7b62d7c10b4a77343346665
Lucas Frederico <lucas.frederico@senior.com.br>
2019-12-09 14:44:17 -0300

    Adicionado aplicação Spring que disponibilizará as APIs. Também adicionado o projeto Flutter.

---
+++ flutter-estacionamento-api/src/main/java/me/lucasrodrigo/flutterestacionamentoapi/FlutterEstacionamentoApiApplication.java
@@ -0,0 +1,13 @@
+package me.lucasrodrigo.flutterestacionamentoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterEstacionamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterEstacionamentoApiApplication.class, args);
+	}
+
+}
---
+++ flutter-estacionamento-api/src/test/java/me/lucasrodrigo/flutterestacionamentoapi/FlutterEstacionamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucasrodrigo.flutterestacionamentoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterEstacionamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
